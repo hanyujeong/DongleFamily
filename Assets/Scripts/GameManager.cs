c120715eb9970a3cd8db82f4f4aa6bc710d89491
y <shootfruit@naver.com>
2025-12-10 23:26:53 +0900

    다음 동글 만들기 전

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,30 +3,42 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Dongle lastDongle;
+    public GameObject donglePrefab;
+    public Transform dongleGroup;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        NextDongle();
     }
 
-    // Update is called once per frame
-    void Update()
+    Dongle GetDongle()
     {
-        
+        GameObject instant = Instantiate(donglePrefab, dongleGroup);
+        Dongle instantDongle = instant.GetComponent<Dongle>();
+        return instantDongle;
     }
+
+    void NextDongle()
+    {
+        Dongle newDongle = GetDongle();
+        lastDongle = newDongle;
+    }
+
 
     public void TouchDown()
-    {        
-        {
-            lastDongle.Drag();
-        }
+    {   if(lastDongle == null) 
+            return;
+
+        lastDongle.Drag();
     }
     public void TouchUp()
     {
-        {
-            lastDongle.Drop();
-        }
+        if(lastDongle == null)
+            return;
+
+        lastDongle.Drop();
+        lastDongle = null;
+
     }
 
 }
