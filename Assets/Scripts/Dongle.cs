79701aa8079d3c451697cd4ac8763a864f152da1
y <shootfruit@naver.com>
2025-12-10 22:03:25 +0900

    x축 경계 설정

--- Assets/Scripts/Dongle.cs
+++ Assets/Scripts/Dongle.cs
@@ -10,9 +10,22 @@ public class Dongle : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {// x position follows mouse position with borders
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float leftBorder = -4.2f + transform.localScale.x / 2f;
+        float rightBorder = 4.2f - transform.localScale.x / 2f;
+
+        if (mousePos.x < leftBorder)
+        {
+            mousePos.x = leftBorder;
+        }
+        else if (mousePos.x > rightBorder)
+        {
+            mousePos.x = rightBorder;
+        }
+        mousePos.y = 8;
         mousePos.z = 0; // Set z to 0 to keep the object in the 2D plane
-        transform.position = mousePos;
+        transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+
     }
 }
