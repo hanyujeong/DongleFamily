7abed44a956fd50bae74e1a9203c86b5af910e45
y <shootfruit@naver.com>
2025-12-11 15:51:49 +0900

    같은 오브젝트가 만나면 머지

--- Assets/Scripts/Dongle.cs
+++ Assets/Scripts/Dongle.cs
@@ -1,16 +1,21 @@
 ﻿using JetBrains.Annotations;
+using System.Collections;
 using UnityEngine;
 
 public class Dongle : MonoBehaviour
 {
     public int level;
     public bool isDrag;
+    public bool isMerge;
+
     Rigidbody2D rigid;
+    CircleCollider2D circle;
     Animator anim;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        circle = GetComponent<CircleCollider2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -41,7 +46,7 @@ public class Dongle : MonoBehaviour
             mousePos.z = 0;
             transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
         }
-       
+
     }
 
     public void Drag()
@@ -53,5 +58,55 @@ public class Dongle : MonoBehaviour
     {
         isDrag = false;
         rigid.simulated = true;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Dongle")
+        {
+            Dongle other = collision.gameObject.GetComponent<Dongle>();
+
+            if (level == other.level && !isMerge && !other.isMerge && level < 7)
+            {
+                // 나와 상대편 위치 가져오기
+                float meX = transform.position.x;
+                float meY = transform.position.y;
+                float otherX = other.transform.position.x;
+                float otherY = other.transform.position.y;
+                //1. 내가 아래에 있을 때
+                //2. 동일한 높이일 때, 내가 오른쪽에 있을 때
+                if (meY < otherY || (meY == otherY && meX > otherX))
+                {//상대방은 숨기기                 
+                    other.Hide(transform.position);
+                    // 나는 레벨업
+                }
+            }
+        }
+    }
+
+    public void Hide(Vector3 targetPos)
+    {
+        isMerge = true;
+
+        rigid.simulated = false;
+        circle.enabled = false;
+
+        StartCoroutine(HideRoutine(targetPos));
+
+    }
+
+    IEnumerator HideRoutine(Vector3 targetPos)
+    {
+        int frameCount = 0;
+
+        while (frameCount < 20)
+        {
+            frameCount++;
+            transform.position = Vector3.Lerp(transform.position, targetPos, 0.5f);
+            yield return null;
+        }
+
+        isMerge = false;
+        gameObject.SetActive(false);
     }
 }
